bd4b0247646d89cd774c99f72559a5cd0f852786
chaze05 <ranirubillos05@gmail.com>
2026-04-28 22:11:12 +0800

    rename migration file

--- clinic-api/database/seeders/ClinicServicesSeeder.php
+++ clinic-api/database/seeders/ClinicServicesSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class ClinicServiceSeeder extends Seeder
+class ClinicServicesSeeder extends Seeder
 {
     public function run(): void
     {
