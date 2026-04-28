b035b2036fab086f752677fe9c5d1b5d2018e12d
chaze05 <ranirubillos05@gmail.com>
2026-04-28 22:35:17 +0800

    update profile seeder

--- clinic-api/database/seeders/DoctorProfileSeeder.php
+++ clinic-api/database/seeders/DoctorProfileSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class DoctorProfilesSeeder extends Seeder
+class DoctorProfileSeeder extends Seeder
 {
     public function run(): void
     {
